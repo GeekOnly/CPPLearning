d257fa8ea96867e2b3cdc57f71d4c63db8deb1a9
GeekOnly <xs13@hotmail.co.th>
2021-05-31 17:52:13 +0700

    First C++ Compile

---
+++ VSCodeC++FirstTestCompile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 0;
+    cout << "Hello Gamer\n";
+    cout << "Enter Numer x Test\n";
+    cin >> x;
+    cout << "x multiply x = " << x*x;
+    return 0;
+}
